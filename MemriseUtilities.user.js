9454e8580b026ec07618ae4e4653ede208267d43
Bryan Creel <scytalezero@users.noreply.github.com>
2015-06-11 09:58:04 -0400

    Various bug fixes

--- MemriseUtilities.user.js
+++ MemriseUtilities.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          MemriseUtilities
 // @namespace     http://ligature.me
-// @version       0.9.2
+// @version       0.9.3
 // @grant         none
 // @description   Various helper functions for Memrise and some other EO sites.
 // @icon          http://cdn.altrn.tv/icons/memrise_10088.png?width=50&height=50&mode=crop&anchor=middlecenter
@@ -13,86 +13,104 @@
 // @copyright     2012+, ScytaleZero
 // ==/UserScript==
 
-switch (document.domain) {
-  case "akademio-de-esperanto.org":
-    $("<input type='button' value='List Words' />")
-      .click(AkademioList)
-      .prependTo("h3.trafonombro");
-    
-    function AkademioList() {
-      $("h3.trafonombro").append("<div><textarea id='Wordlist' style='width:800px; height:400px;'></textarea></div>");
-      $("#serchorezultotabelo tr").each(function(Index) {
-        if (Index === 0) return;
-        $("#Wordlist").append( $($(this).find("td")[1]).text() + "\t" + $($(this).find("td")[4]).text() + "\n" );
-      });
-    }
-    break;
-  case "www.memrise.com":
-    if ( ($("li.active > a.tab").length === 0) || ($(".container > h2").length > 0) ) {
-      //This isn't a course levels page
-      return;
-    }
+var Wordlist, Promises, CourseTag;
 
-    var Wordlist = {};
-    var Promises = [];
-    var CourseTag = SanitizeTag($("h1.course-name").text().trim())
-
-    //Insert the terms extractor button
-    $("ul.nav-pills").append("<li><a id='WordListButton'>Word list</a></li>");
-    $("#WordListButton").click(SpiderLevels);
-
-    //Iterate through the levels and kick off the retrieves.
-    function SpiderLevels() {
-      //Add our UI
-      $("div.container-main").prepend("<div id='WordListContainer'><h2>Working</h2></div>");
-      $("a.level").each(function(Index) {
-        var URL = "http://www.memrise.com" + $(this).attr("href");
-        console.log("Dispatching URL: " + URL);
-        //if (Index > 0) return; //Debug
-        Promises.push($.ajax({
-          dataType: "html",
-          url: URL
-        }).done(ExtractTerms));
-      });
+function Main() {
+  switch (document.domain) {
+    case "akademio-de-esperanto.org":
+      Out("Processing for akademio de esperanto");
+      $("<input type='button' value='List Words' />")
+        .click(AkademioList)
+        .prependTo("h3.trafonombro");
       
-      $.when.apply($, Promises).done(SpiderDone);
-    }
-
-    //All levels have been parsed and the wordlist is done.
-    function SpiderDone() {
-      var WordlistText = "", LCV;
-      
-      console.log(Wordlist);
-      //Add the wordlist text area.
-      $("#WordListContainer").empty().append("<div><h2>Course Wordlist as TSV</h2><textarea id='Wordlist' style='width:800px; height:400px;'></textarea><p>&nbsp;</p></div>");
-      for (var Level in Wordlist) {
-        for (LCV=0; LCV<Wordlist[Level].length; LCV++) {
-          WordlistText += Wordlist[Level][LCV].Word + "\t" + Wordlist[Level][LCV].Translation + "\t" + 
-            CourseTag + "," + SanitizeTag(Level) + "\n";
-        }
-      }
-      $("#Wordlist").val(WordlistText.trimRight());
-    }
-
-    //Pull words and definitions from a level page.
-    function ExtractTerms(Data) {
-      var LevelName = $(Data).find("strong.level-number").text().trim();
-      console.log("Level: " + LevelName);
-      Wordlist[LevelName] = [];
-      $(Data).find("div.text-text").each(function() {
-        Wordlist[LevelName].push({
-          "Word": $(this).find("div.col_a").text(),
-          "Translation": $(this).find("div.col_b").text()
+      function AkademioList() {
+        $("h3.trafonombro").append("<div><textarea id='Wordlist' style='width:800px; height:400px;'></textarea></div>");
+        $("#serchorezultotabelo tr").each(function(Index) {
+          if (Index === 0) return;
+          $("#Wordlist").append( $($(this).find("td")[1]).text() + "\t" + $($(this).find("td")[4]).text() + "\n" );
         });
-      });
-      $("#WordListContainer h2").append(".");
-    }
+      }
+      break;
+    case "www.memrise.com":
+      Out("Processing for Memrise");
+      if ( ($("li.active > a.tab").length === 0) || ($(".container > h2").length > 0) ) {
+        //This isn't a course levels page
+        Out("Not a course levels page.");
+        return;
+      }
 
-    //Format text appropriately for LWT tags.
-    function SanitizeTag(Buffer) {
-      var Tag = Buffer.replace(/[^\w]/g, "");
-      if (Tag.length > 20) Tag = Tag.substring(0, 20);
-      return Tag;
-    }
-    break;
+      Wordlist = {};
+      Promises = [];
+      CourseTag = SanitizeTag($("h1.course-name").text().trim());
+
+      //Insert the terms extractor button
+      $("ul.nav-pills").append("<li><a id='WordListButton'>Word list</a></li>");
+      $("#WordListButton").click(SpiderLevels);
+      break;
+    default:
+      Out("Page not recognized.");
+  }
 }
+
+/* Memrise Functions */
+//Iterate through the levels and kick off the retrieves.
+function SpiderLevels() {
+  //Add our UI
+  $("div.container-main").prepend("<div id='WordListContainer'><h2>Working</h2></div>");
+  $("a.level").each(function(Index) {
+    var URL = "http://www.memrise.com" + $(this).attr("href");
+    Out("Dispatching URL: " + URL);
+    //if (Index > 0) return; //Debug
+    Promises.push($.ajax({
+      dataType: "html",
+      url: URL
+    }).done(ExtractTerms));
+  });
+  
+  $.when.apply($, Promises).done(SpiderDone);
+}
+
+//All levels have been parsed and the wordlist is done.
+function SpiderDone() {
+  var WordlistText = "", LCV;
+  
+  Out("Wordlist parsed.");
+  //Add the wordlist text area.
+  $("#WordListContainer").empty().append("<div><h2>Course Wordlist as TSV</h2><textarea id='Wordlist' style='width:800px; height:400px;'></textarea><p>&nbsp;</p></div>");
+  for (var Level in Wordlist) {
+    for (LCV=0; LCV<Wordlist[Level].length; LCV++) {
+      WordlistText += Wordlist[Level][LCV].Word + "\t" + Wordlist[Level][LCV].Translation + "\t" + 
+        CourseTag + "," + SanitizeTag(Level) + "\n";
+    }
+  }
+  $("#Wordlist").val(WordlistText.trimRight());
+}
+
+//Pull words and definitions from a level page.
+function ExtractTerms(Data) {
+  var LevelName = $(Data).find("strong.level-number").text().trim();
+  Out("Level: " + LevelName);
+  Wordlist[LevelName] = [];
+  $(Data).find("div.text-text").each(function() {
+    Wordlist[LevelName].push({
+      "Word": $(this).find("div.col_a").text(),
+      "Translation": $(this).find("div.col_b").text()
+    });
+  });
+  $("#WordListContainer h2").append(".");
+}
+
+//Format text appropriately for LWT tags.
+function SanitizeTag(Buffer) {
+  var Tag = Buffer.replace(/[^\w]/g, "");
+  if (Tag.length > 20) Tag = Tag.substring(0, 20);
+  return Tag;
+}
+
+/* Utility Functions */
+function Out(Buffer) {
+  if (console.log)
+    console.log("[MUtils] " + Buffer);
+}
+
+Main();
